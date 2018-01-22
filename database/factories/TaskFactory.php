15ef8bac2919d331ef2f2eab5655fc9f74068d9b
Nemanja Ivankovic <064necko5@gmail.com>
2018-01-22 04:34:10 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -5,12 +5,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Task::class, function (Faker $faker) {
     return [
-        'user_id' => function()
-        {
+        'user_id' => function () {
             return factory(User::class)->create()->id;
         },
         'title' => $faker->sentence,
         'description' => $faker->text,
-        'status' => $faker->numberBetween(0,2)
+        'status' => $faker->numberBetween(0, 2),
     ];
 });
